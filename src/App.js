97affb7d879ac5e19aed8cbd8c6f9c2f640da4bd
Vilena Martirosova <vilenamartirosova@gmail.com>
2022-09-09 21:01:58 +0100

    Added dictionary API integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main className="App-main">
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          This project was created by Vilena Martirosova and is open-sourced on
+          GitHub
+        </footer>
+      </div>
     </div>
   );
 }
