e55869a4e47cf370871cc821a64511e18eeac4fe
Vilena Martirosova <vilenamartirosova@gmail.com>
2022-09-10 23:49:42 +0100

    Added audio player

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,22 @@
 import React from "react";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank">
-        Listen
-      </a>
-      <br />
-      {props.phonetic.text}
-    </div>
-  );
+  if (props.phonetic.audio) {
+    return (
+      <div className="phonetic mt-3 mb-1">
+        <div>
+          <ReactAudioPlayer
+            className="audio-player"
+            src={props.phonetic.audio}
+            autoPlay={false}
+            controls={true}
+          />
+        </div>
+        <div>{props.phonetic.text}</div>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
